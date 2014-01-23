c0d2546745e4aa4304ec372669d31153a3625496
randalmaile <randalmaile@gmail.com>
2014-01-22 20:37:13 -0800

    upgraded Bootstrap + hashtag#index layout/presentation changes

--- Gemfile
+++ Gemfile
@@ -9,6 +9,7 @@ group :development do
   gem 'sqlite3'
   gem 'faker'
   gem 'pry-rails'
+  gem 'rails_layout'
 end
 
 group :development, :test do
@@ -26,7 +27,7 @@ end
 group :assets do
   gem 'sass-rails',   '~> 3.2.3'
   gem 'coffee-rails', '~> 3.2.1'
-  gem 'bootstrap-sass', '~> 2.3.1.0'
+  gem 'bootstrap-sass', '~> 3.0.3.0'
   gem 'uglifier', '>= 1.0.3'
 
   # See https://github.com/sstephenson/execjs#readme for more supported runtimes
--- app/models/bookmark.rb
+++ app/models/bookmark.rb
@@ -1,23 +1,24 @@
 class Bookmark < ActiveRecord::Base
-  attr_accessible :description, :title, :url, :tagmaps_attributes
+  attr_accessible :description, :title, :url, :tags_string
   belongs_to :user
-  has_many :tagmaps, inverse_of: :bookmark
-  has_many :hashtags, :through => :tagmaps
-  accepts_nested_attributes_for :tagmaps, allow_destroy: true
-  # accepts_nested_attributes_for :tagmaps, dependent: destroy
-
-
-  # after_destroy :destroy_orphaned_children
+  has_many :tagmaps
+  has_many :hashtags, :through => :tagmaps, dependent: :destroy
 
   validates :url, :format => URI::regexp(%w(http https)), presence: true
   validates :title, presence: true
 
 
+
+  def tags_string
+    self.hashtags.map {|h| h.tag}.join(" ")
+  end
+
+  def tags_string=(values)
+    # this is how you change the relation - right through the join table.
+    self.hashtags = values.split(" ").map { |s| Hashtag.where(tag: s).first_or_create }
+  end
+
   private
 
-  # def destroy_orphaned_children
-  #   hashtags.each do |hashtag|
-  #     hashtag.destroy if hashtag.bookmarks.empty?
-  #   end
-  # end
+
 end

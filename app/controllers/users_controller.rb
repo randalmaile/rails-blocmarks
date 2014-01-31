7bf284eaa1ca8cb192dd669417ed7c4f8ddf6f8b
randalmaile <randalmaile@gmail.com>
2014-01-31 00:21:45 -0800

    changed user sign up and sign in flow

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -19,7 +19,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(params[:user])
     if @user.save
-      redirect_to root_url, :notice => "Signed up!"
+      redirect_to log_in_path, :notice => "Signed up! Start adding Blocmarks!"
     else
       render :new
     end
